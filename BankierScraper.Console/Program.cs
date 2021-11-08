41c11d3edf39a37787b2e9d7db56f0bd25884c21
Tomasz Cekało <tomasz.cekalo@gmail.com>
2021-11-08 13:45:17 +0100

    added hotnews scraping using anglesharp

---
+++ BankierScraper.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace BankierScraper.Console
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            await new MainPageScraper().Scrape();
+        }
+    }
+}
--- BankierScraper/MainPageScraper.cs
+++ BankierScraper/MainPageScraper.cs
@@ -1,4 +1,10 @@
-﻿namespace BankierScraper
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankierScraper
 {
     public class MainPageScraper : IMainPageScraper
     {
@@ -13,5 +19,31 @@
         //Business
         //Satellites
         //
+
+        public async Task Scrape()
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var address = "https://www.bankier.pl/";
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(address);
+            var cellSelector = "#hotnews-box a";
+            var cells = document.QuerySelectorAll(cellSelector);
+            //var titles = cells.Select(m => m);
+            var articles = cells.OfType<IUrlUtilities>();
+            //AngleSharp.Html.Dom.HtmlAnchorElement;
+            //AngleSharp.Html.Dom.HtmlUrlBaseElement
+            var items = cells
+                .Select(x => new
+                {
+                    x.TextContent,
+                    Href = x.GetAttribute("href")
+                });
+            //var items = articles.Select(x => new
+            //{
+            //    x.Href,
+            //    //x.
+            //});
+            Console.ReadKey();
+        }
     }
 }
