d4c831accd8fce5d1ac780b276060a5ffc65ec20
Tomasz Cekało <tomasz.cekalo@gmail.com>
2021-11-13 20:52:36 +0100

    added some code

--- BankierScraper/MainPageScraper.cs
+++ BankierScraper/MainPageScraper.cs
@@ -9,7 +9,7 @@ namespace BankierScraper
     public class MainPageScraper : IMainPageScraper
     {
         //HotNews
-        //DilyNews
+        //DailyNews
         //HomeQuote
         //QuotationOfTheDay
         //HomeSpecialTopic
@@ -32,18 +32,33 @@ namespace BankierScraper
             var articles = cells.OfType<IUrlUtilities>();
             //AngleSharp.Html.Dom.HtmlAnchorElement;
             //AngleSharp.Html.Dom.HtmlUrlBaseElement
+            ScrapeHotNews(document);
+            ScrapeDailyNews(document);
+            Console.ReadKey();
+        }
+
+        public void ScrapeHotNews(IDocument document)
+        {
+            var cellSelector = "#hotnews-box a";
+            var cells = document.QuerySelectorAll(cellSelector);
             var items = cells
                 .Select(x => new
                 {
                     x.TextContent,
                     Href = x.GetAttribute("href")
                 });
-            //var items = articles.Select(x => new
-            //{
-            //    x.Href,
-            //    //x.
-            //});
-            Console.ReadKey();
+        }
+        public object ScrapeDailyNews(IDocument document)
+        {
+            var cellSelector = "#home-dailynews-box .m-title-with-label-item:not(.-orange)";
+            var cells = document.QuerySelectorAll(cellSelector);
+            var items = cells
+                .Select(x => new
+                {
+                    x.QuerySelector(".m-title-with-label-item__title").TextContent,
+                    Href = x.GetAttribute("href")
+                });
+            return items;
         }
     }
 }
